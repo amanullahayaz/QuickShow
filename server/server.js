d137a57a8789acf99a60410c66d8b98cecd73732
MOhammad Amanullah <amanullahayaz2005@gmail.com>
2026-02-01 23:54:31 +0530

    userSchema

--- server/server.js
+++ server/server.js
@@ -2,16 +2,20 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './configs/db.js';
-
-dotenv.config(); // LOAD ONCE, HERE ONLY
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+
 // Middleware
 app.use(express.json());
 app.use(cors());
-
+app.use(clerkMiddleware);
 // Connect DB
 await connectDB();
 
@@ -19,6 +23,8 @@ await connectDB();
 app.get('/', (req, res) => {
   res.send('i am home');
 });
+// Set up the "/api/inngest" (recommended) routes with the serve handler
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.listen(port, () => {
   console.log(`server is listening at port number ${port}`);
