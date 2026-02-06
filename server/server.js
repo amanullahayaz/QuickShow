f07a08dbaa826187b6f1083d727d211da6501350
MOhammad Amanullah <amanullahayaz2005@gmail.com>
2026-02-06 22:05:12 +0530

    setting up now playing movies api

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express';
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
+import showRouter from './routes/showRoutes.js';
 
 dotenv.config();
 const port=3000;
@@ -23,11 +24,8 @@ app.get('/', async (req, res) => {
   res.send('i am home');
 });
 
-app.use("/api/inngest", async (req, res, next) => {
-  await connectDB();
-  next();
-}, serve({ client: inngest, functions }));
-
+app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use('/api/show',showRouter);
 
 app.listen(port,(req,res)=>{
   console.log(`App is listening to the port number ${port}`);
