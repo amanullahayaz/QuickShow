9b7011e9794bafa1a7438e863bad0f7c52f15263
MOhammad Amanullah <amanullahayaz2005@gmail.com>
2026-02-22 15:50:19 +0530

    connected frontend with backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,8 +14,12 @@ import Dashboard from './pages/Admin/Dashboard';
 import ListShows from './pages/Admin/ListShows';
 import AddShows from './pages/Admin/AddShows';
 import ListBookings from './pages/Admin/ListBookings';
+import { useAppContext } from './context/AppContext';
+import { SignIn } from '@clerk/clerk-react';
 const App = () => {
   const isAdminRoute = useLocation().pathname.startsWith('/admin')
+
+  const {user}=useAppContext()
   return (
     <>
       <Toaster />
@@ -27,7 +31,11 @@ const App = () => {
         <Route path='/movies/:id/:date' element={<SeatLayout />} />
         <Route path='/my-bookings' element={<MyBookings />} />
         <Route path='/favorite' element={<Favorite />} />
-        <Route path='/admin/*' element={<Layout/>}>
+        <Route path='/admin/*' element={user? <Layout/> : (
+          <div className='min-h-screen flex justify-center items-center'>
+            <SignIn fallbackRedirectUrl={'/admin'}/>
+          </div>
+        )}>
           <Route index element={<Dashboard />} />
           <Route path="add-shows" element={<AddShows />} />
           <Route path="list-shows" element={<ListShows />} />
