e3a1584723a5bde6ecdc09eb5ee5d271ea8e3cba
MOhammad Amanullah <amanullahayaz2005@gmail.com>
2026-03-01 13:38:48 +0530

    made dashboard functional on real data

--- client/src/pages/Admin/Dashboard.jsx
+++ client/src/pages/Admin/Dashboard.jsx
@@ -1,13 +1,13 @@
 import { ChartLineIcon, CircleDollarSign, CircleDollarSignIcon, PlayCircleIcon, StarIcon, UserIcon } from 'lucide-react';
 import React, { useEffect, useState } from 'react'
-import { dummyDashboardData } from '../../assets/assets';
 import Loading from '../../components/Loading';
 import Title from '../../components/Admin/Title';
 import BlurCircle from '../../components/BlurCircle';
 import { dateFormat } from '../../lib/dateFormat';
-
+import { useAppContext } from '../../context/AppContext';
+import toast from 'react-hot-toast';
 const Dashboard = () => {
-
+    const {axios,getToken,user,image_base_url}=useAppContext();
     const currency=import.meta.env.VITE_CURRENCY
     const [dashboardData,setDashboardData]=useState({
         totalBookings:0,
@@ -30,13 +30,27 @@ const Dashboard = () => {
     ]
     useEffect(()=>{
     const fetchDashboardData=async()=>{
-        setDashboardData(dummyDashboardData)
+        try{
+            const {data} = await axios.get('/api/admin/dashboard',{headers : {
+                Authorization : `Bearer ${await getToken()}`
+            }})
+
+            if(data.success){
+                setDashboardData(data.dashboardData)
+                setLoading(false)
+            }else{
+              toast.error(data.message)  
+            }
+        }catch(error){
+            toast.error("Error fetching dashboard data ",error)
+        }
         setLoading(false)
     };
-    
-    
-        fetchDashboardData();
-    },[]);
+        if(user){
+             fetchDashboardData();
+        }
+       
+    },[user]);
 
 
   return !loading? (
@@ -65,7 +79,7 @@ const Dashboard = () => {
             <div key={show._id} className='w-55 rounded-lg overflow-hidden 
             h-full pb-3 bg-primary/10 border border-primary/20 hover:-translate-y-1
             transition duration-300'>
-                <img src={show.movie.poster_path} alt="" className='h-60 w-full object-cover'/>
+                <img src={image_base_url+show.movie.poster_path} alt="" className='h-60 w-full object-cover'/>
                 <p className='font-medium p-2 truncate'>{show.movie.title}</p>
                 <div className='flex items-center justify-between px-2'>
                     <p className='text-lg font-medium'>{currency}{show.showPrice}</p>
