cb72ce6399724ddacd31414270dbc4133608e51b
MOhammad Amanullah <amanullahayaz2005@gmail.com>
2026-04-04 23:32:40 +0530

    fn name corrected

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -90,7 +90,7 @@ const releaseSeatsAndDeleteBooking = inngest.createFunction(
 
 
 const sendBookingConfirmationEmail = inngest.createFunction(
-  { id: 'send-booking-consfirmation-email' },
+  { id: 'send-booking-confirmation-email' },
   { event: "app/show.booked" },
 
   async ({ event, step }) => {
